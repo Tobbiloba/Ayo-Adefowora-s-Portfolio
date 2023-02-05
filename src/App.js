67f11f543c6ddce1b6b5604135b4dd56961b056d
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-02-05 18:24:01 +0100

    added about page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import MenuBar from "./components/Menubar";
 import Mainpage from "./components/MainPage";
+import AboutMe from "./components/AboutMe";
 function App() {
   return (
     <div className="bg-black">
      <MenuBar />
      <Mainpage />
+     <AboutMe />
     </div>
   );
 }
