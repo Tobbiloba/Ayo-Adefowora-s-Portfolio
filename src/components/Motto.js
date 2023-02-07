f1c574b095e5b23c8c2cc265fb9912e7ca7cf715
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-02-07 12:22:45 +0100

    finished motto

---
+++ src/components/Motto.js
@@ -0,0 +1,17 @@
+function Motto() {
+    return(
+        <div className="w-[100vw] h-[40vh] bg-amber-500 flex flex-col justify-center items-center">
+            <div className="lg:w-[65vw] w-[90vw]">
+                <h1 className="text-xl text-white font-semibold">My Motto</h1>
+            </div>
+            <div className="lg:w-[65vw] w-[90vw] text-center px-4 mt-6 lg:mt-12">
+                <h1 className="lg:text-7xl text-4xl font-bold font-serif text-white">
+                    "Designing with Precision. Improving
+                    User Journeys. Crafting Exceptional 
+                    Experiences."
+                </h1>
+            </div>
+        </div>
+    )
+}
+export default Motto;
