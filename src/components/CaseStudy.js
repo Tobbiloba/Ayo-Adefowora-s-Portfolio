03b4878ea00805ba658dfdaf1c1ba195ceb4743b
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-02-08 12:00:06 +0100

    change

--- src/components/CaseStudy.js
+++ src/components/CaseStudy.js
@@ -7,7 +7,7 @@ function CaseStudy() {
             </div>
 
             <div className="mt-12 ">
-                <div className="flex lg:flex-row flex-col-reverse lg:w-[65vw] lg:h-[50vh] w-[90vw]">
+                <div className="flex lg:flex-row flex-col-reverse lg:w-[65vw] lg:h-[52.5vh] w-[90vw] overflow-hidden">
                     <div className="flex flex-col lg:w-[30vw] w-[90vw] h-[50vh]">
                         <h1 className="text-white text-5xl mt-5 font-mono font-bold">Foodlink</h1>
                         <p className="text-white lg:w-[20vw] w-[90vw] text-2xl mt-20 leading-10">A food truck app designed for meeting user needs
@@ -23,7 +23,7 @@ function CaseStudy() {
                     </div>
                 </div>
 
-                 <div className="flex lg:flex-row flex-col lg:w-[65vw] w-[90vw] lg:h-[50vh] mt-36">
+                 <div className="flex lg:flex-row overflow-hidden flex-col lg:w-[65vw] w-[90vw] lg:h-[50vh] mt-36">
                     <div className="lg:w-[40vw] w-[90vw] lg:h-[50vh] h-[45vh] bg-amber-500">
                         <img className="mt-12 relative right-12 lg:right-32" src="/laptop1.png" alt="phone"/>
                     </div>
@@ -39,7 +39,7 @@ function CaseStudy() {
                     </div>
                 </div>
 
-                 <div className="flex lg:flex-row flex-col-reverse lg:w-[65vw] w-[90vw] lg:h-[50vh] mt-36">
+                 <div className="flex lg:flex-row overflow-hidden flex-col-reverse lg:w-[65vw] w-[90vw] lg:h-[50vh] mt-36">
                     <div className="flex flex-col w-[90vw] lg:w-[30vw]">
                         <h1 className="text-white text-5xl lg:mt-5 mt-8 font-mono font-bold">Yu Mei</h1>
                         <p className="text-white lg:w-[20vw] w-[90vw] text-2xl lg:mt-20 mt-10 leading-10">
